f829df98c163b654b4da0efb2464a89863dad67c
Kaushal Kishor <kkishor@lason.com>
2020-01-31 11:25:26 +0530

    User registration page with validators added

--- src/app/modules/user-management/user-registration/user-registration.component.ts
+++ src/app/modules/user-management/user-registration/user-registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-registration',
@@ -6,10 +7,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-registration.component.scss']
 })
 export class UserRegistrationComponent implements OnInit {
+ model: userDetails;
+  
+  userForm;
+  submitted:boolean;
+
+  // convenience getter for easy access to form fields
+    get f() { return this.userForm.controls; }
+
+
+  onClickSubmit(data) { 
+    this.submitted = true;
+        if(this.userForm.invalid){
+          return;
+        }
+        console.log(data)
+        console.log("Form submitted")
+    }
 
   constructor() { }
 
+   roles:String[];
+   sizes:String[];
   ngOnInit() {
-  }
+    this.submitted = false;
+    this.roles = ['Developer', 'Data Scientist'];
+    this.sizes = ["Small","Medium","Large","Extra-Large","Tent"];
+    this.userForm = new FormGroup({
+                emailid: new FormControl('',Validators.required),
+                fullname: new FormControl('',Validators.required),
+                passwd: new FormControl('',Validators.required),
+                number: new FormControl(''),
+                gitId: new FormControl('',Validators.required),
+                roles: new FormControl('',Validators.required),
+                path: new FormControl('',),
+                size: new FormControl('',)
 
+    });
+    }
+  
+}
+
+export interface userDetails {
+    name: String,
+    designation: String,
+    email: String,
+    address: String,
+    contact: String,
+    filelocation: String
 }
--- src/app/modules/user-management/user-registration/user-registration.module.ts
+++ src/app/modules/user-management/user-registration/user-registration.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { UserRegistrationRoutingModule } from './user-registration-routing.module';
 import { UserRegistrationComponent } from './user-registration.component';
@@ -8,6 +9,7 @@ import { UserRegistrationComponent } from './user-registration.component';
   declarations: [UserRegistrationComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     UserRegistrationRoutingModule
   ]
 })
