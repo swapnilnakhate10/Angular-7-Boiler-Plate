733cafb0d97c7dabb8c298742788debc47cb21c7
RohitWadke <rohit.wadke@exelaonline.com>
2020-01-31 18:49:12 +0530

    Create event changes

--- src/app/constants/url-details.ts
+++ src/app/constants/url-details.ts
@@ -6,7 +6,7 @@ export class UrlDetails {
     public static users: string = environment.hackathonCore + "users/";
     public static eventsListByOrganizerId: string = environment.hackathonCore + "events/organizer/5e33ba4a779fee2fa862bdaf";
 
-    public static events: string = environment.hackathonCore + "events/";
+    public static createEvent: string = environment.hackathonCore + "events/";
     public static teams: string = environment.hackathonCore + "teams/";
     public static organizers: string = environment.hackathonCore + "organizers/";
     public static prizes: string = environment.hackathonCore + "prizes/";
