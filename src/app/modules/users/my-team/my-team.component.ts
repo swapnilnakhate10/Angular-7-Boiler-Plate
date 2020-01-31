33261c678a54e47956997ce0e182b8a0a93a7941
Kaushal Kishor <kkishor@lason.com>
2020-01-31 19:27:07 +0530

    My-team UI updated

--- src/app/modules/users/my-team/my-team.component.ts
+++ src/app/modules/users/my-team/my-team.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-my-team',
@@ -8,19 +9,54 @@ import { Component, OnInit } from '@angular/core';
 export class MyTeamComponent implements OnInit {
 
   isTeamCreate:boolean = false;
+  submitted:boolean = false;
+  users:any;
+
   teams:any;
 
-  //Methods
+  teamForm;
+
+  // convenience getter for easy access to form fields
+    get f() { return this.teamForm.controls; }
+
+  //Button create click event to create new Team
   createTeam() {
     this.isTeamCreate = true;
+    this.submitted = false;
   }
+
+  //Cancel button click event
+  cancel(){
+    this.submitted = false;
+    this.isTeamCreate = false;
+  
+  }
+
+  //Form submit event
+  register(formData){
+    this.submitted = true;
+    if(this.teamForm.invalid)
+     return;
+     console.log(formData);
+  }
+
   constructor() { 
+    this.users = [
+      {id:1,name:"Kaushal Kishor",designation:"Developer"},
+      {id:2,name:"Ashish Kumar",designation:"Data Scientist"}];
+
     this.teams = [
       {id:1,name:"Code Blooded",team_leader:"Amrish"},
-      {id:2,name:"Runtime Terror",team_leader:"Kaushal"}];
+      {id:2,name:"Runtime Terror",team_leader:"Kaushal Kishor"}];
   }
 
   ngOnInit() {
+     this.teamForm = new FormGroup({
+                teamname: new FormControl('',Validators.required),
+                tagline: new FormControl('Programming rocks!'),
+                path: new FormControl('',) ,
+                demoURL: new FormControl('')
+     });
   }
 
 }
