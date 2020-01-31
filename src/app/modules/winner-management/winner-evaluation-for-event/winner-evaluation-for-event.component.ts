27fc1d08a2387107c049c2004890ca7064ae061e
LuckyVaishnav <Lucky.Vaishnav@BANCTECINDIA.IN>
2020-01-31 15:16:20 +0530

    event evaluation compo

---
+++ src/app/modules/winner-management/winner-evaluation-for-event/winner-evaluation-for-event.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-winner-evaluation-for-event',
+  templateUrl: './winner-evaluation-for-event.component.html',
+  styleUrls: ['./winner-evaluation-for-event.component.scss']
+})
+export class WinnerEvaluationForEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
