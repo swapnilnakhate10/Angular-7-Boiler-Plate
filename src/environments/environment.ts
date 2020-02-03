3dc58327eef2c59f61d0d0affdd7c106cbb4aff3
Amaresh Kumar <amaresh.kumar@BANCTECINDIA.IN>
2020-02-03 20:53:41 +0530

    env changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  hackathonCore : 'http://sbr-devp-129:8080/api/',
-  githubCore : 'http://localhost:3000/'
+  hackathonCore : 'https://cb-hck-core.exela.global/',
+  githubCore : 'https://github.com/SwapnilNakhate/'
 };
