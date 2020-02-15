e3559c129e7c95c7729f8a51f977f9cf3e03e65d
krish <krish.ghatul007@gmail.com>
2020-02-15 13:04:11 +0530

    chat integration with teletype - done

--- src/app/modules/shared/components/footer/footer.component.ts
+++ src/app/modules/shared/components/footer/footer.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  iframechat: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  chatIconToggle() {
+    this.iframechat = !this.iframechat;
+  }
 }
