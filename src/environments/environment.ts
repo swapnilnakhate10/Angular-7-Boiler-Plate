b8efed6fcd95ecd13e11dafb448456b2af6a5631
Amaresh Kumar <amaresh.kumar@BANCTECINDIA.IN>
2020-09-08 11:55:11 +0530

    Updating link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  // hackathonCore : 'https://cb-hck-core.exela.global/api/'
-  hackathonCore : 'http://localhost:8080/api/'
+    hackathonCore : 'http://cb-hck-core.exela.global/api/'
+  //hackathonCore : 'http://localhost:8080/api/'
 };
